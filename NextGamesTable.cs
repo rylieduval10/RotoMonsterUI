12bdeedc27d5376fa02945e1f29b98e5e79c68d8
Rylie DuVal <rylieduval10@gmail.com>
2026-05-19 09:20:07 -0500

    Add NextGamesTable component

---
+++ NextGamesTable.cs
@@ -0,0 +1,42 @@
+using HtmlTags;
+
+namespace RotoMonsterUI
+{
+    public class NextGamesTable
+    {
+        private string _tableHtml;
+        private string _title;
+
+        public NextGamesTable(string tableHtml)
+        {
+            _tableHtml = tableHtml;
+        }
+
+        public NextGamesTable WithTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+
+        public string Render()
+        {
+            var wrapper = new HtmlTag("div").AddClass("next-games-wrapper");
+
+            if (!string.IsNullOrEmpty(_title))
+            {
+                var title = new HtmlTag("h3")
+                    .AddClass("next-games-title")
+                    .Text(_title);
+                wrapper.Append(title);
+            }
+
+            var tableWrapper = new HtmlTag("div")
+                .AddClass("next-games-table-wrapper")
+                .AppendHtml(_tableHtml);
+
+            wrapper.Append(tableWrapper);
+
+            return wrapper.ToString();
+        }
+    }
+}
