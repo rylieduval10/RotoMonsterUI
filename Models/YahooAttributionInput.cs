91eed1373359d75eb44edce8c1956ccd95eba9c2
Rylie DuVal <rylieduval10@gmail.com>
2026-07-09 14:44:10 -0500

    Add YahooAttribution compliance component with inline SVG logo

---
+++ Models/YahooAttributionInput.cs
@@ -0,0 +1,9 @@
+namespace RotoMonsterUI
+{
+    public class YahooAttributionInput
+    {
+        public string LinkUrl { get; set; } = "https://sports.yahoo.com/fantasy/";
+
+        public int LogoHeight { get; set; } = 16;
+    }
+}
