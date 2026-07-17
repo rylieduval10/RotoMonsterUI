0036e310862ae09eac2485737b809a3525e931f2
Rylie DuVal <rylieduval10@gmail.com>
2026-07-17 12:07:29 -0500

    Add AvailablePlayers list property to PollOptionPlayerPickerInput and GiveGetPlayerPickerInput - holds the searchable player pool, doesn't wire up search yet

--- Models/GiveGetPlayerPickerInput.cs
+++ Models/GiveGetPlayerPickerInput.cs
@@ -8,6 +8,8 @@ namespace RotoMonsterUI
         public bool CanEdit { get; set; } = true;
         public List<DisplayPlayerInput> GivePlayers { get; set; } = new List<DisplayPlayerInput>();
         public List<DisplayPlayerInput> GetPlayers { get; set; } = new List<DisplayPlayerInput>();
+        public List<DisplayPlayerInput> AvailableGivePlayers { get; set; } = new List<DisplayPlayerInput>();
+        public List<DisplayPlayerInput> AvailableGetPlayers { get; set; } = new List<DisplayPlayerInput>();
         public string GiveSearchPlaceholder { get; set; } = "Search your roster...";
         public string GetSearchPlaceholder { get; set; } = "Search all players...";
     }
--- Models/PollOptionPlayerPickerInput.cs
+++ Models/PollOptionPlayerPickerInput.cs
@@ -6,6 +6,7 @@ namespace RotoMonsterUI
     {
         public string Id { get; set; }
         public List<DisplayPlayerInput> AddedPlayers { get; set; } = new List<DisplayPlayerInput>();
+        public List<DisplayPlayerInput> AvailablePlayers { get; set; } = new List<DisplayPlayerInput>();
         public bool CanEdit { get; set; } = true;
     }
 }
